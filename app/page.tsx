c45ceaafb0761660b24da707971ff867ef0ab0b0
Abhinav Robinson <abhinavrobinson@gmail.com>
2023-06-26 15:55:48 +0530

    feature: nextpages

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,20 @@ import ImageComponent from '@/components/ImageComponent'
 import useHomeApi from '@/queries/useHomeApi'
 
 export default function Home() {
-  const { data } = useHomeApi()
+  const { data, fetchNextPage } = useHomeApi()
   return (
     <main className="h-full w-full">
       <HealthCheck />
       <h1 className="my-16 py-10 text-center text-6xl font-bold">ImaginAI</h1>
       <div className="mx-auto mb-10 w-full max-w-5xl columns-3 gap-5 space-y-5 p-5 pb-10">
-        {data?.url_list?.map((url) => (
-          <ImageComponent key={url} url={url} />
-        ))}
+        {data?.pages
+          .reduce(
+            (acc, current) => acc.concat(current.url_list),
+            [] as string[]
+          )
+          .map((url) => (
+            <ImageComponent key={url} url={url} />
+          ))}
         {!data &&
           Array.from({ length: 9 }).map((_, i) => (
             <div
@@ -22,6 +27,12 @@ export default function Home() {
             />
           ))}
       </div>
+      <button
+        className="mx-auto my-6 flex max-w-max items-center gap-2 rounded-full border border-black/10 bg-black/5 px-3 py-1.5 text-sm dark:border-white/50 dark:bg-white/10"
+        onClick={() => void fetchNextPage()}
+      >
+        Fetch More (in development)
+      </button>
     </main>
   )
 }
--- queries/useHomeApi.ts
+++ queries/useHomeApi.ts
@@ -1,22 +1,21 @@
 'use client'
 
 import routes from '@/api/constants'
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 
-export default function useHomeApi(
-  variables: (typeof routes)['home']['variables'] = {
-    cursor: Math.floor(Math.random() * 2000),
-  }
-) {
-  const results = useQuery({
-    queryFn: async () => {
-      return (await fetch(`/${routes.home.path}`, {
-        headers: routes.home.headers,
-        method: routes.home.method,
-        body: JSON.stringify(variables),
-      }).then((res) => res.json())) as (typeof routes)['home']['body']
-    },
-    queryKey: [routes.home.path, variables],
+const fetchItems = async ({ cursor }: { cursor: number }) => {
+  return (await fetch(`/${routes.home.path}`, {
+    headers: routes.home.headers,
+    method: routes.home.method,
+    body: JSON.stringify({ cursor }),
+  }).then((res) => res.json())) as (typeof routes)['home']['body']
+}
+
+export default function useHomeApi() {
+  const results = useInfiniteQuery({
+    queryFn: (v) => fetchItems({ cursor: (v.pageParam as number) ?? 0 }),
+    queryKey: [routes.home.path],
+    getNextPageParam: (lastPage) => lastPage.next_cursor,
   })
 
   return { ...results }
